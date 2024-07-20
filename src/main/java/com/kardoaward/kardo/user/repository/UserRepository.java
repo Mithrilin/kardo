72e90c274a96dd14ee895bc4e093b6e52f45c646
DmitriyDemintsev <d.a.demintsev@yandex.ru>
2024-07-20 13:54:13 +0300

    добавить ветку addException добавить пакет exception с классами добавить метод findByEmail

--- src/main/java/com/kardoaward/kardo/user/repository/UserRepository.java
+++ src/main/java/com/kardoaward/kardo/user/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.kardoaward.kardo.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+//    Optional<NewUserRequest> findByEmail(String email);
 }
