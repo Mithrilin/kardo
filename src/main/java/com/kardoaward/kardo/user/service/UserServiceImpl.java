e9d502b5042c81d9eceae9b1e562de59a9e0c6d2
DmitriyDemintsev <d.a.demintsev@yandex.ru>
2024-07-21 19:57:19 +0300

    rebase на develope

--- src/main/java/com/kardoaward/kardo/user/service/UserServiceImpl.java
+++ src/main/java/com/kardoaward/kardo/user/service/UserServiceImpl.java
@@ -10,6 +10,7 @@ import com.kardoaward.kardo.user.service.helper.UserValidationHelper;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -28,9 +29,7 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
-
     private PasswordEncoder passwordEncoder;
-
     private final UserRepository userRepository;
 
     private final UserMapper userMapper;
