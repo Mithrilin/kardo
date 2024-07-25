8dffcf075351a0c07be62932ce7b7bc9eccef80b
Roman <romanukyan86@yandex.ru>
2024-07-25 19:11:50 +0300

    добавить класс UpdateOfflineCompetitionRequest;

---
+++ src/main/java/com/kardoaward/kardo/offlineCompetition/model/dto/UpdateOfflineCompetitionRequest.java
@@ -0,0 +1,33 @@
+package com.kardoaward.kardo.offlineCompetition.model.dto;
+
+import com.kardoaward.kardo.offlineCompetition.model.enums.EventStatus;
+import com.kardoaward.kardo.offlineCompetition.model.enums.Field;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.kardoaward.kardo.offlineCompetition.model.enums.EventStatus.UPCOMING;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateOfflineCompetitionRequest {
+
+    @Size(min = 2, max = 250, message = "Длина title должна быть в диапазоне от 2 до 250 символов.")
+    private String title;
+    @Future(message = "CompetitionStart должен быть в будущем.")
+    private LocalDate competitionStart;
+    @Future(message = "CompetitionEnd должен быть в будущем.")
+    private LocalDate competitionEnd;
+    private EventStatus status = UPCOMING;
+    @Size(min = 2, max = 250, message = "Длина location должна быть в диапазоне от 2 до 250 символов.")
+    private String location;
+    private List<Field> fields;
+    @Size(min = 2, max = 5000, message = "Длина Description должна быть в диапазоне от 2 до 5000 символов.")
+    private String description;
+}
