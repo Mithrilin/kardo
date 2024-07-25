7d1eb94ccfd054934d9eca60b317d4c625cdc4a7
Roman <romanukyan86@yandex.ru>
2024-07-25 18:35:17 +0300

    добавить класс OfflineCompetition;

---
+++ src/main/java/com/kardoaward/kardo/offlineCompetition/model/OfflineCompetition.java
@@ -0,0 +1,56 @@
+package com.kardoaward.kardo.offlineCompetition.model;
+
+import com.kardoaward.kardo.offlineCompetition.model.enums.EventStatus;
+import com.kardoaward.kardo.offlineCompetition.model.enums.Field;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "offline_competitions")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class OfflineCompetition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String hashtag;
+    @Column(name = "competition_start")
+    private LocalDate competitionStart;
+    @Column(name = "competition_end")
+    private LocalDate competitionEnd;
+    @Enumerated(EnumType.STRING)
+    private EventStatus status;
+    private String location;
+    @ElementCollection
+    @CollectionTable(name="offline_competition_fields",
+            joinColumns=@JoinColumn(name="offline_competition_id"))
+    @Column(name="field")
+    @Enumerated(EnumType.STRING)
+/*  ToDo
+     После добавления контроллера проверить сохраняются/возвращаются ли значения в этом поле.
+ */
+    private List<Field> fields;
+    private String description;
+}
