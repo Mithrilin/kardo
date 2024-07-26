45134e7e9bbe6ad1f19a6b6f5d29438f993a8166
Roman <romanukyan86@yandex.ru>
2024-07-27 01:20:28 +0300

    добавить класс CommentDto;

---
+++ src/main/java/com/kardoaward/kardo/comment/model/dto/CommentDto.java
@@ -0,0 +1,21 @@
+package com.kardoaward.kardo.comment.model.dto;
+
+import com.kardoaward.kardo.user.model.dto.UserDto;
+import com.kardoaward.kardo.videoClip.model.VideoClip;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDto {
+
+    private Long id;
+    private UserDto authorDto;
+    private LocalDateTime publicationTime;
+    private VideoClip videoClip;
+    private String text;
+}
