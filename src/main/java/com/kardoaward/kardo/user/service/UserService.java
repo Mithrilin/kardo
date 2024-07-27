5dc9d2a7bca61fd3d348e3c7b53a8893584540d5
Roman <romanukyan86@yandex.ru>
2024-07-27 23:46:23 +0300

    добавить метод getContestantsBySelectionId;

--- src/main/java/com/kardoaward/kardo/user/service/UserService.java
+++ src/main/java/com/kardoaward/kardo/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.kardoaward.kardo.user.service;
 
 import com.kardoaward.kardo.user.model.User;
 import com.kardoaward.kardo.user.model.dto.UpdateUserRequest;
+import com.kardoaward.kardo.user.model.dto.UserShortDto;
 
 import java.util.List;
 
@@ -16,4 +17,6 @@ public interface UserService {
     List<User> getUsersByIds(List<Long> ids, int from, int size);
 
     User updateUser(Long userId, UpdateUserRequest request);
+
+    List<UserShortDto> getContestantsBySelectionId(Long selectionId, int from, int size);
 }
