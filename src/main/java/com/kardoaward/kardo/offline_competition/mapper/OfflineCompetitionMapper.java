0ad3d7654eae4454c1b7a433c508412d6e3d80b0
Roman <romanukyan86@yandex.ru>
2024-07-27 14:02:23 +0300

    добавить класс OfflineCompetitionMapper;

---
+++ src/main/java/com/kardoaward/kardo/offline_competition/mapper/OfflineCompetitionMapper.java
@@ -0,0 +1,11 @@
+package com.kardoaward.kardo.offline_competition.mapper;
+
+import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
+import com.kardoaward.kardo.offline_competition.model.dto.NewOfflineCompetitionRequest;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface OfflineCompetitionMapper {
+
+
+}
