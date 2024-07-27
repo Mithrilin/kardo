ff58263c5b65733910e58d4964738fbdc8b0ece6
Roman <romanukyan86@yandex.ru>
2024-07-27 14:13:37 +0300

    добавить метод OfflineCompetitionToOfflineCompetitionDto в OfflineCompetitionMapper;

--- src/main/java/com/kardoaward/kardo/offline_competition/mapper/OfflineCompetitionMapper.java
+++ src/main/java/com/kardoaward/kardo/offline_competition/mapper/OfflineCompetitionMapper.java
@@ -2,10 +2,13 @@ package com.kardoaward.kardo.offline_competition.mapper;
 
 import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
 import com.kardoaward.kardo.offline_competition.model.dto.NewOfflineCompetitionRequest;
+import com.kardoaward.kardo.offline_competition.model.dto.OfflineCompetitionDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface OfflineCompetitionMapper {
 
     OfflineCompetition newOfflineCompetitionRequestToOfflineCompetition(NewOfflineCompetitionRequest newCompetition);
+
+    OfflineCompetitionDto OfflineCompetitionToOfflineCompetitionDto(OfflineCompetition returnedCompetition);
 }
