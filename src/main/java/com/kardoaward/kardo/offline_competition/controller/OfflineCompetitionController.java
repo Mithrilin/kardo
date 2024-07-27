77d215edbdb857be5b8f5a3d5caf38af617a80c2
Roman <romanukyan86@yandex.ru>
2024-07-27 13:04:24 +0300

    добавить класс OfflineCompetitionController;

---
+++ src/main/java/com/kardoaward/kardo/offline_competition/controller/OfflineCompetitionController.java
@@ -0,0 +1,17 @@
+package com.kardoaward.kardo.offline_competition.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/competitions/offline")
+@Validated
+public class OfflineCompetitionController {
+
+
+}
