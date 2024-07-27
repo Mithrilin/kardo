9561e06321c48d02941ec57014100cd01478ce99
Roman <romanukyan86@yandex.ru>
2024-07-27 18:06:20 +0300

    добавить метод newSelectionRequestToSelection в SelectionMapper;

--- src/main/java/com/kardoaward/kardo/selection/mapper/SelectionMapper.java
+++ src/main/java/com/kardoaward/kardo/selection/mapper/SelectionMapper.java
@@ -1,7 +1,14 @@
 package com.kardoaward.kardo.selection.mapper;
 
+import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
+import com.kardoaward.kardo.selection.model.Selection;
+import com.kardoaward.kardo.selection.model.dto.NewSelectionRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SelectionMapper {
+
+    @Mapping(source = "competition", target = "competition")
+    Selection newSelectionRequestToSelection(NewSelectionRequest newSelectionRequest, OfflineCompetition competition);
 }
