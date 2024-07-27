182f8696226addaea7baf71a0d1f9e5662163a7a
Roman <romanukyan86@yandex.ru>
2024-07-27 18:09:27 +0300

    добавить метод addSelection;

--- src/main/java/com/kardoaward/kardo/selection/service/SelectionService.java
+++ src/main/java/com/kardoaward/kardo/selection/service/SelectionService.java
@@ -1,4 +1,9 @@
 package com.kardoaward.kardo.selection.service;
 
+import com.kardoaward.kardo.selection.model.dto.NewSelectionRequest;
+import com.kardoaward.kardo.selection.model.dto.SelectionDto;
+
 public interface SelectionService {
+
+    SelectionDto addSelection(NewSelectionRequest newSelectionRequest);
 }
