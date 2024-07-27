07521423eb788154e68cc0549a76e5dc1cfa8c99
Roman <romanukyan86@yandex.ru>
2024-07-27 18:09:27 +0300

    добавить метод addSelection;

--- src/main/java/com/kardoaward/kardo/selection/controller/admin/SelectionAdminController.java
+++ src/main/java/com/kardoaward/kardo/selection/controller/admin/SelectionAdminController.java
@@ -1,8 +1,14 @@
 package com.kardoaward.kardo.selection.controller.admin;
 
+import com.kardoaward.kardo.selection.model.dto.NewSelectionRequest;
+import com.kardoaward.kardo.selection.model.dto.SelectionDto;
+import com.kardoaward.kardo.selection.service.SelectionService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin/selections")
 @Validated
 public class SelectionAdminController {
+
+    private final SelectionService selectionService;
+
+    @PostMapping
+    public SelectionDto createSelection(@RequestBody @Valid NewSelectionRequest newSelectionRequest) {
+        log.info("Добавление администратором нового отбора {}.", newSelectionRequest);
+        return selectionService.addSelection(newSelectionRequest);
+    }
 }
--- src/main/java/com/kardoaward/kardo/selection/service/SelectionServiceImpl.java
+++ src/main/java/com/kardoaward/kardo/selection/service/SelectionServiceImpl.java
@@ -1,5 +1,12 @@
 package com.kardoaward.kardo.selection.service;
 
+import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
+import com.kardoaward.kardo.selection.mapper.SelectionMapper;
+import com.kardoaward.kardo.selection.model.Selection;
+import com.kardoaward.kardo.selection.model.dto.NewSelectionRequest;
+import com.kardoaward.kardo.selection.model.dto.SelectionDto;
+import com.kardoaward.kardo.selection.repository.SelectionRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +15,22 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class SelectionServiceImpl implements SelectionService {
+
+    private final SelectionRepository selectionRepository;
+
+    private final SelectionMapper selectionMapper;
+
+    @Override
+    @Transactional
+    public SelectionDto addSelection(NewSelectionRequest newSelectionRequest) {
+        /* ToDo
+            Исправить. Добавить хелпер.
+         */
+        OfflineCompetition competition = null;
+        Selection selection = selectionMapper.newSelectionRequestToSelection(newSelectionRequest, competition);
+        Selection returnedSelection = selectionRepository.save(selection);
+        SelectionDto selectionDto = selectionMapper.selectionToSelectionDto(returnedSelection);
+        log.info("Отбор с ID = {} создан.", selectionDto.getId());
+        return selectionDto;
+    }
 }
