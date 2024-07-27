d39d301e8fd21116f1376d9fb84ab7882d842c92
Roman <romanukyan86@yandex.ru>
2024-07-27 23:05:14 +0300

    добавить метод updateSelection в SelectionMapper;

--- src/main/java/com/kardoaward/kardo/selection/mapper/SelectionMapper.java
+++ src/main/java/com/kardoaward/kardo/selection/mapper/SelectionMapper.java
@@ -4,10 +4,15 @@ import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
 import com.kardoaward.kardo.selection.model.Selection;
 import com.kardoaward.kardo.selection.model.dto.NewSelectionRequest;
 import com.kardoaward.kardo.selection.model.dto.SelectionDto;
+import com.kardoaward.kardo.selection.model.dto.UpdateSelectionRequest;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
+
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
 @Mapper(componentModel = "spring")
 public interface SelectionMapper {
@@ -18,4 +23,7 @@ public interface SelectionMapper {
     SelectionDto selectionToSelectionDto(Selection returnedSelection);
 
     List<SelectionDto> selectionListToSelectionDtoList(List<Selection> selections);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = IGNORE)
+    void updateSelection(UpdateSelectionRequest request, @MappingTarget Selection selection);
 }
