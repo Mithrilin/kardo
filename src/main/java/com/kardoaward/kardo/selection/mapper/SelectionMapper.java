c9228429670bb92501aeea3a8d6a26b2a2377912
Roman <romanukyan86@yandex.ru>
2024-07-27 18:06:43 +0300

    добавить метод selectionToSelectionDto в SelectionMapper;

--- src/main/java/com/kardoaward/kardo/selection/mapper/SelectionMapper.java
+++ src/main/java/com/kardoaward/kardo/selection/mapper/SelectionMapper.java
@@ -3,6 +3,7 @@ package com.kardoaward.kardo.selection.mapper;
 import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
 import com.kardoaward.kardo.selection.model.Selection;
 import com.kardoaward.kardo.selection.model.dto.NewSelectionRequest;
+import com.kardoaward.kardo.selection.model.dto.SelectionDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,4 +12,6 @@ public interface SelectionMapper {
 
     @Mapping(source = "competition", target = "competition")
     Selection newSelectionRequestToSelection(NewSelectionRequest newSelectionRequest, OfflineCompetition competition);
+
+    SelectionDto selectionToSelectionDto(Selection returnedSelection);
 }
