92a12257df32eebd84b22f55d1924a9a2fb1cbd3
Roman <romanukyan86@yandex.ru>
2024-07-27 15:11:56 +0300

    добавить метод updateOfflineCompetition в OfflineCompetitionMapper;

--- src/main/java/com/kardoaward/kardo/offline_competition/mapper/OfflineCompetitionMapper.java
+++ src/main/java/com/kardoaward/kardo/offline_competition/mapper/OfflineCompetitionMapper.java
@@ -3,9 +3,14 @@ package com.kardoaward.kardo.offline_competition.mapper;
 import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
 import com.kardoaward.kardo.offline_competition.model.dto.NewOfflineCompetitionRequest;
 import com.kardoaward.kardo.offline_competition.model.dto.OfflineCompetitionDto;
+import com.kardoaward.kardo.offline_competition.model.dto.UpdateOfflineCompetitionRequest;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
+
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
 @Mapper(componentModel = "spring")
 public interface OfflineCompetitionMapper {
@@ -15,4 +20,7 @@ public interface OfflineCompetitionMapper {
     OfflineCompetitionDto offlineCompetitionToOfflineCompetitionDto(OfflineCompetition returnedCompetition);
 
     List<OfflineCompetitionDto> offlineCompetitionListToOfflineCompetitionDtoList(List<OfflineCompetition> competitions);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = IGNORE)
+    void updateOfflineCompetition(UpdateOfflineCompetitionRequest request, @MappingTarget OfflineCompetition competition);
 }
