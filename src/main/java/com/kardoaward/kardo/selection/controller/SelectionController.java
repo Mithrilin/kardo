5a6d95be1cca3254e4c75f7647e187b8ea5cbb7d
Roman <romanukyan86@yandex.ru>
2024-07-27 23:05:14 +0300

    добавить метод updateSelection в SelectionMapper;

--- src/main/java/com/kardoaward/kardo/selection/controller/SelectionController.java
+++ src/main/java/com/kardoaward/kardo/selection/controller/SelectionController.java
@@ -32,7 +32,7 @@ public class SelectionController {
 
     @GetMapping
     public List<SelectionDto> getSelections(@RequestParam(defaultValue = "0") @Min(0) int from,
-                                           @RequestParam(defaultValue = "10") @Positive int size) {
+                                            @RequestParam(defaultValue = "10") @Positive int size) {
         log.info("Получение списка отборов.");
         return selectionService.getSelections(from, size);
     }
