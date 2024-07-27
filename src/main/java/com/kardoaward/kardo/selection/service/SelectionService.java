c85508e16ac916582bf5499b6f2404d48e4d80d4
Roman <romanukyan86@yandex.ru>
2024-07-27 22:33:02 +0300

    добавить метод getSelections;

--- src/main/java/com/kardoaward/kardo/selection/service/SelectionService.java
+++ src/main/java/com/kardoaward/kardo/selection/service/SelectionService.java
@@ -3,6 +3,8 @@ package com.kardoaward.kardo.selection.service;
 import com.kardoaward.kardo.selection.model.dto.NewSelectionRequest;
 import com.kardoaward.kardo.selection.model.dto.SelectionDto;
 
+import java.util.List;
+
 public interface SelectionService {
 
     SelectionDto addSelection(NewSelectionRequest newSelectionRequest);
@@ -10,4 +12,6 @@ public interface SelectionService {
     void deleteSelection(Long selectionId);
 
     SelectionDto getSelectionById(Long selectionId);
+
+    List<SelectionDto> getSelections(int from, int size);
 }
