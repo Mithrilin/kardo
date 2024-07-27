61e8576ab4f050d58ead34e5e294158f45518367
Roman <romanukyan86@yandex.ru>
2024-07-27 18:05:44 +0300

    добавить интерфейс SelectionRepository;

---
+++ src/main/java/com/kardoaward/kardo/selection/repository/SelectionRepository.java
@@ -0,0 +1,7 @@
+package com.kardoaward.kardo.selection.repository;
+
+import com.kardoaward.kardo.selection.model.Selection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SelectionRepository extends JpaRepository<Selection, Long> {
+}
