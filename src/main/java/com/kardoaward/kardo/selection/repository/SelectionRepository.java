f88e5ded61a9ebcea3a9b01d4e626d0eeb18346a
Roman <romanukyan86@yandex.ru>
2024-07-31 01:21:40 +0300

    добавить SQL запросы в SelectionRepository и UserRepository;

--- src/main/java/com/kardoaward/kardo/selection/repository/SelectionRepository.java
+++ src/main/java/com/kardoaward/kardo/selection/repository/SelectionRepository.java
@@ -4,16 +4,19 @@ import com.kardoaward.kardo.selection.model.Selection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SelectionRepository extends JpaRepository<Selection, Long> {
 
-    /* ToDo
-        Написать SQL запрос.
-     */
+    @Query(value =
+            "SELECT * " +
+            "FROM selections AS s " +
+            "WHERE s.id IN (SELECT pr.selection_id " +
+                           "FROM participation_requests AS pr " +
+                           "WHERE pr.requester_id = :requestorId " +
+                           "AND pr.status = 'CONFIRMED') " +
+            "ORDER BY s.id", nativeQuery = true)
     Page<Selection> findAllByRequestorId(Long requestorId, PageRequest pageRequest);
 
-    /* ToDo
-        Написать SQL запрос.
-     */
-    Page<Selection> findAllByOfflineCompetitionId(Long competitionId, PageRequest pageRequest);
+    Page<Selection> findByCompetition_Id(Long competitionId, PageRequest pageRequest);
 }
--- src/main/java/com/kardoaward/kardo/user/repository/UserRepository.java
+++ src/main/java/com/kardoaward/kardo/user/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.kardoaward.kardo.user.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,8 +12,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Page<User> findByIdIn(List<Long> ids, PageRequest pageRequest);
 
-    /* ToDo
-        Написать SQL запрос.
-     */
+    @Query(value =
+            "SELECT * " +
+            "FROM users AS u " +
+            "WHERE u.id IN (SELECT pr.requester_id " +
+                           "FROM participation_requests AS pr " +
+                           "WHERE pr.selection_id = :selectionId " +
+                           "AND pr.status = 'CONFIRMED') " +
+            "ORDER BY u.id", nativeQuery = true)
     Page<User> findAllBySelectionId(Long selectionId, PageRequest pageRequest);
 }
