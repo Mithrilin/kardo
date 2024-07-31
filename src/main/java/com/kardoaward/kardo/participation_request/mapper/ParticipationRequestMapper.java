4e493edfc19bbf6c063edfc727e4e7935ed78557
Roman <romanukyan86@yandex.ru>
2024-07-31 16:27:42 +0300

    добавить метод newParticipationRequestToParticipationRequest в ParticipationRequestMapper;

--- src/main/java/com/kardoaward/kardo/participation_request/mapper/ParticipationRequestMapper.java
+++ src/main/java/com/kardoaward/kardo/participation_request/mapper/ParticipationRequestMapper.java
@@ -1,8 +1,22 @@
 package com.kardoaward.kardo.participation_request.mapper;
 
+import com.kardoaward.kardo.participation_request.model.ParticipationRequest;
+import com.kardoaward.kardo.participation_request.model.dto.NewParticipationRequest;
+import com.kardoaward.kardo.selection.mapper.SelectionMapper;
+import com.kardoaward.kardo.selection.model.Selection;
+import com.kardoaward.kardo.user.mapper.UserMapper;
+import com.kardoaward.kardo.user.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {UserMapper.class, SelectionMapper.class})
 public interface ParticipationRequestMapper {
 
+    @Mapping(target = "id", constant = "0L")
+    @Mapping(target = "status", constant = "PENDING")
+    @Mapping(source = "selection", target = "selection")
+    @Mapping(source = "user", target = "requester")
+    ParticipationRequest newParticipationRequestToParticipationRequest(NewParticipationRequest newParticipationRequest,
+                                                                       User user,
+                                                                       Selection selection);
 }
