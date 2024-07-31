7969bb6b1af644ea6a0c4ec4d1633ad998b47a32
Roman <romanukyan86@yandex.ru>
2024-07-31 14:21:05 +0300

    добавить интерфейс OnlineCompetitionMapper;

---
+++ src/main/java/com/kardoaward/kardo/online_competition/mapper/OnlineCompetitionMapper.java
@@ -0,0 +1,23 @@
+package com.kardoaward.kardo.online_competition.mapper;
+
+import com.kardoaward.kardo.online_competition.model.OnlineCompetition;
+import com.kardoaward.kardo.online_competition.model.dto.NewOnlineCompetitionRequest;
+import com.kardoaward.kardo.online_competition.model.dto.OnlineCompetitionDto;
+import com.kardoaward.kardo.online_competition.model.dto.UpdateOnlineCompetitionRequest;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
+@Mapper(componentModel = "spring")
+public interface OnlineCompetitionMapper {
+
+
+
+
+
+
+}
