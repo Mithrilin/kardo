76a292a5e9dd792f145448a627afb8347eb4b32e
Roman <romanukyan86@yandex.ru>
2024-07-31 16:03:38 +0300

    добавить класс ParticipationRequestAdminController;

---
+++ src/main/java/com/kardoaward/kardo/participation_request/controller/admin/ParticipationRequestAdminController.java
@@ -0,0 +1,15 @@
+package com.kardoaward.kardo.participation_request.controller.admin;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/admin/participations")
+@Validated
+public class ParticipationRequestAdminController {
+}
