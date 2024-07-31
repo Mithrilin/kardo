a41001a2cf82c2120828353c6c3eceb6d7048b46
Roman <romanukyan86@yandex.ru>
2024-07-31 14:19:20 +0300

    добавить класс OnlineCompetitionAdminController;

---
+++ src/main/java/com/kardoaward/kardo/online_competition/controller/admin/OnlineCompetitionAdminController.java
@@ -0,0 +1,35 @@
+package com.kardoaward.kardo.online_competition.controller.admin;
+
+import com.kardoaward.kardo.online_competition.model.dto.NewOnlineCompetitionRequest;
+import com.kardoaward.kardo.online_competition.model.dto.OnlineCompetitionDto;
+import com.kardoaward.kardo.online_competition.model.dto.UpdateOnlineCompetitionRequest;
+import com.kardoaward.kardo.online_competition.service.OnlineCompetitionService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/admin/competitions/online")
+@Validated
+public class OnlineCompetitionAdminController {
+
+    private final OnlineCompetitionService service;
+
+
+
+
+
+
+
+}
