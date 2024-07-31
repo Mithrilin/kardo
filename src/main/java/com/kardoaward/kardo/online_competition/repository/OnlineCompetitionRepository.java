e18101509de09021aba102ab8e06125458ce528d
Roman <romanukyan86@yandex.ru>
2024-07-31 14:21:27 +0300

    добавить интерфейс OnlineCompetitionRepository;

---
+++ src/main/java/com/kardoaward/kardo/online_competition/repository/OnlineCompetitionRepository.java
@@ -0,0 +1,7 @@
+package com.kardoaward.kardo.online_competition.repository;
+
+import com.kardoaward.kardo.online_competition.model.OnlineCompetition;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OnlineCompetitionRepository extends JpaRepository<OnlineCompetition, Long> {
+}
