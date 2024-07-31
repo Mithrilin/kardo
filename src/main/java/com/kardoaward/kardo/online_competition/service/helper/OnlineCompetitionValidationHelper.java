ed7410c983280d7bbb52717bb73f3c812324678c
Roman <romanukyan86@yandex.ru>
2024-07-31 14:23:06 +0300

    добавить метод isOnlineCompetitionPresent в OnlineCompetitionValidationHelper;

--- src/main/java/com/kardoaward/kardo/online_competition/service/helper/OnlineCompetitionValidationHelper.java
+++ src/main/java/com/kardoaward/kardo/online_competition/service/helper/OnlineCompetitionValidationHelper.java
@@ -16,5 +16,15 @@ public class OnlineCompetitionValidationHelper {
 
     private final OnlineCompetitionRepository repository;
 
+    public OnlineCompetition isOnlineCompetitionPresent(Long onlineCompetitionId) {
+        Optional<OnlineCompetition> optionalOnlineCompetition = repository.findById(onlineCompetitionId);
 
+        if (optionalOnlineCompetition.isEmpty()) {
+            log.error("Онлайн-соревнование с ИД {} отсутствует в БД.", onlineCompetitionId);
+            throw new NotFoundException(String.format("Онлайн-соревнование с ИД %d отсутствует в БД.",
+                    onlineCompetitionId));
+        }
+
+        return optionalOnlineCompetition.get();
+    }
 }
