68c23503f13f3bbe9f56cc4f77d1be1e90771fc6
Roman <romanukyan86@yandex.ru>
2024-07-31 14:21:47 +0300

    добавить интерфейс OnlineCompetitionService;

---
+++ src/main/java/com/kardoaward/kardo/online_competition/service/OnlineCompetitionService.java
@@ -0,0 +1,17 @@
+package com.kardoaward.kardo.online_competition.service;
+
+import com.kardoaward.kardo.online_competition.model.dto.NewOnlineCompetitionRequest;
+import com.kardoaward.kardo.online_competition.model.dto.OnlineCompetitionDto;
+import com.kardoaward.kardo.online_competition.model.dto.UpdateOnlineCompetitionRequest;
+
+import java.util.List;
+
+public interface OnlineCompetitionService {
+
+
+
+
+
+
+
+}
