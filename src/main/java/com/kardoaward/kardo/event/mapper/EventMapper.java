eec9995ba240288075113549ea949114794bbb49
Roman <romanukyan86@yandex.ru>
2024-08-03 01:52:33 +0300

    добавить метод updateEvent в EventMapper;

--- src/main/java/com/kardoaward/kardo/event/mapper/EventMapper.java
+++ src/main/java/com/kardoaward/kardo/event/mapper/EventMapper.java
@@ -3,12 +3,17 @@ package com.kardoaward.kardo.event.mapper;
 import com.kardoaward.kardo.event.model.Event;
 import com.kardoaward.kardo.event.model.dto.EventDto;
 import com.kardoaward.kardo.event.model.dto.NewEventRequest;
+import com.kardoaward.kardo.event.model.dto.UpdateEventRequest;
 import com.kardoaward.kardo.offline_competition.mapper.OfflineCompetitionMapper;
 import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
+
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
 @Mapper(componentModel = "spring", uses = OfflineCompetitionMapper.class)
 public interface EventMapper {
@@ -26,4 +31,7 @@ public interface EventMapper {
     EventDto eventToEventDto(Event returnedEvent);
 
     List<EventDto> eventListToEventDtoList(List<Event> events);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = IGNORE)
+    void updateEvent(UpdateEventRequest request, @MappingTarget Event event);
 }
