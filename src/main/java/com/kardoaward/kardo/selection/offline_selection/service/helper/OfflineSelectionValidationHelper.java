8d422a71db753943bd4fa34aea431714165f722b
Roman <romanukyan86@yandex.ru>
2024-08-02 20:30:45 +0300

    добавить метод isOfflineSelectionPresent в OfflineSelectionValidationHelper;

--- src/main/java/com/kardoaward/kardo/selection/offline_selection/service/helper/OfflineSelectionValidationHelper.java
+++ src/main/java/com/kardoaward/kardo/selection/offline_selection/service/helper/OfflineSelectionValidationHelper.java
@@ -1,11 +1,29 @@
 package com.kardoaward.kardo.selection.offline_selection.service.helper;
 
+import com.kardoaward.kardo.exception.NotFoundException;
+import com.kardoaward.kardo.selection.offline_selection.model.OfflineSelection;
+import com.kardoaward.kardo.selection.offline_selection.repository.OfflineSelectionRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class OfflineSelectionValidationHelper {
+
+    private final OfflineSelectionRepository offlineSelectionRepository;
+
+    public OfflineSelection isOfflineSelectionPresent(Long selectionId) {
+        Optional<OfflineSelection> optionalOfflineSelection = offlineSelectionRepository.findById(selectionId);
+
+        if (optionalOfflineSelection.isEmpty()) {
+            log.error("Оффлайн-отбор с ИД {} отсутствует в БД.", selectionId);
+            throw new NotFoundException(String.format("Оффлайн-отбор с ИД %d отсутствует в БД.", selectionId));
+        }
+
+        return optionalOfflineSelection.get();
+    }
 }
