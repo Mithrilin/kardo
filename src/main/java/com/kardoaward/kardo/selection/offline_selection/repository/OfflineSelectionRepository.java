9254d1c2b4fb58ad486b45f7dd2f16185dc775eb
Roman <romanukyan86@yandex.ru>
2024-08-02 20:18:11 +0300

    добавить интерфейс OfflineSelectionRepository;

---
+++ src/main/java/com/kardoaward/kardo/selection/offline_selection/repository/OfflineSelectionRepository.java
@@ -0,0 +1,7 @@
+package com.kardoaward.kardo.selection.offline_selection.repository;
+
+import com.kardoaward.kardo.selection.offline_selection.model.OfflineSelection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfflineSelectionRepository extends JpaRepository<OfflineSelection, Long> {
+}
