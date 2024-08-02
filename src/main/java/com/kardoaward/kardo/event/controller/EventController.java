c125b6c9a0f894db131b32ab2f90d2e52a8aa9fe
Roman <romanukyan86@yandex.ru>
2024-08-02 23:34:57 +0300

    переименовать класс EventController;

---
+++ src/main/java/com/kardoaward/kardo/event/controller/EventController.java
@@ -0,0 +1,15 @@
+package com.kardoaward.kardo.event.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/events")
+@Validated
+public class EventController {
+}
