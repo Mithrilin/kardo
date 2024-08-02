c71250475255ecf60df63a8e5481ef9f4954c012
Roman <romanukyan86@yandex.ru>
2024-08-02 20:49:28 +0300

    добавить метод isUpdateOfflineSelectionDateValid в OfflineSelectionValidationHelper;

--- src/main/java/com/kardoaward/kardo/selection/offline_selection/service/helper/OfflineSelectionValidationHelper.java
+++ src/main/java/com/kardoaward/kardo/selection/offline_selection/service/helper/OfflineSelectionValidationHelper.java
@@ -1,7 +1,9 @@
 package com.kardoaward.kardo.selection.offline_selection.service.helper;
 
+import com.kardoaward.kardo.exception.BadRequestException;
 import com.kardoaward.kardo.exception.NotFoundException;
 import com.kardoaward.kardo.selection.offline_selection.model.OfflineSelection;
+import com.kardoaward.kardo.selection.offline_selection.model.dto.UpdateOfflineSelectionRequest;
 import com.kardoaward.kardo.selection.offline_selection.repository.OfflineSelectionRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,5 +27,29 @@ public class OfflineSelectionValidationHelper {
         }
 
         return optionalOfflineSelection.get();
+    }
+
+    public void isUpdateOfflineSelectionDateValid(OfflineSelection offlineSelection,
+                                                  UpdateOfflineSelectionRequest request) {
+        if (request.getSelectionStart() != null
+                && request.getSelectionEnd() != null
+                && request.getSelectionEnd().isBefore(request.getSelectionStart())) {
+            log.error("Дата начала оффлайн-отбора не может быть после его конца.");
+            throw new BadRequestException("Дата начала оффлайн-отбора не может быть после его конца.");
+        }
+
+        if (request.getSelectionStart() != null
+                && request.getSelectionEnd() == null
+                && request.getSelectionStart().isAfter(offlineSelection.getSelectionEnd())) {
+            log.error("Дата начала оффлайн-отбора не может быть после его конца.");
+            throw new BadRequestException("Дата начала оффлайн-отбора не может быть после его конца.");
+        }
+
+        if (request.getSelectionStart() == null
+                && request.getSelectionEnd() != null
+                && request.getSelectionEnd().isBefore(offlineSelection.getSelectionStart())) {
+            log.error("Дата конца оффлайн-отбора не может быть раньше его начала.");
+            throw new BadRequestException("Дата конца оффлайн-отбора не может быть раньше его начала.");
+        }
     }
 }
