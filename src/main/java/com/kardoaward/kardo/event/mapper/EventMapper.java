a6ebdae86f3331a610080f6860e98aa401725257
Roman <romanukyan86@yandex.ru>
2024-08-02 23:54:17 +0300

    добавить метод newEventRequestToEvent в EventMapper;

--- src/main/java/com/kardoaward/kardo/event/mapper/EventMapper.java
+++ src/main/java/com/kardoaward/kardo/event/mapper/EventMapper.java
@@ -1,7 +1,20 @@
 package com.kardoaward.kardo.event.mapper;
 
+import com.kardoaward.kardo.event.model.Event;
+import com.kardoaward.kardo.event.model.dto.NewEventRequest;
+import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EventMapper {
+
+    @Mapping(target = "id", constant = "0L")
+    @Mapping(target = "status", constant = "UPCOMING")
+    @Mapping(source = "newEventRequest.title", target = "title")
+    @Mapping(source = "offlineCompetition", target = "competition")
+    @Mapping(source = "newEventRequest.location", target = "location")
+    @Mapping(source = "newEventRequest.fields", target = "fields")
+    @Mapping(source = "newEventRequest.description", target = "description")
+    Event newEventRequestToEvent(NewEventRequest newEventRequest, OfflineCompetition offlineCompetition);
 }
