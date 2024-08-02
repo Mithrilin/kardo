6aa5bffa3e6ed0d43cf8abfa98ee7899c43eb345
Roman <romanukyan86@yandex.ru>
2024-08-03 00:01:54 +0300

    добавить метод eventToEventDto в EventMapper;

--- src/main/java/com/kardoaward/kardo/event/mapper/EventMapper.java
+++ src/main/java/com/kardoaward/kardo/event/mapper/EventMapper.java
@@ -1,12 +1,14 @@
 package com.kardoaward.kardo.event.mapper;
 
 import com.kardoaward.kardo.event.model.Event;
+import com.kardoaward.kardo.event.model.dto.EventDto;
 import com.kardoaward.kardo.event.model.dto.NewEventRequest;
+import com.kardoaward.kardo.offline_competition.mapper.OfflineCompetitionMapper;
 import com.kardoaward.kardo.offline_competition.model.OfflineCompetition;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = OfflineCompetitionMapper.class)
 public interface EventMapper {
 
     @Mapping(target = "id", constant = "0L")
@@ -17,4 +19,7 @@ public interface EventMapper {
     @Mapping(source = "newEventRequest.fields", target = "fields")
     @Mapping(source = "newEventRequest.description", target = "description")
     Event newEventRequestToEvent(NewEventRequest newEventRequest, OfflineCompetition offlineCompetition);
+
+    @Mapping(source = "returnedEvent.competition", target = "competitionDto")
+    EventDto eventToEventDto(Event returnedEvent);
 }
