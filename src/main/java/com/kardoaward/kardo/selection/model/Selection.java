bfa6389a63fea9f0f8598bd3fc634d47ab129696
Roman <romanukyan86@yandex.ru>
2024-08-02 19:42:54 +0300

    добавить класс VideoSelection наследник от Selection;

--- src/main/java/com/kardoaward/kardo/selection/model/Selection.java
+++ src/main/java/com/kardoaward/kardo/selection/model/Selection.java
@@ -1,13 +1,8 @@
 package com.kardoaward.kardo.selection.model;
 
-import com.kardoaward.kardo.enums.Field;
 import com.kardoaward.kardo.enums.Status;
 import com.kardoaward.kardo.grand_competition.model.GrandCompetition;
-import com.kardoaward.kardo.selection.model.enums.SelectionType;
-import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
@@ -16,35 +11,26 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import static com.kardoaward.kardo.enums.Status.UPCOMING;
 
-@Entity
-@Table(name = "selections")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-@ToString
+@MappedSuperclass
 public class Selection {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    private String hashtag;
-    @Enumerated(EnumType.STRING)
-    @Column(name = "selection_type")
-    private SelectionType selectionType;
     @ManyToOne(fetch = FetchType.LAZY)
     @ToString.Exclude
     @JoinColumn(name = "competition_id")
@@ -55,11 +41,4 @@ public class Selection {
     private LocalDate selectionEnd;
     @Enumerated(EnumType.STRING)
     private Status status = UPCOMING;
-    @ElementCollection
-    @CollectionTable(name="selection_fields",
-            joinColumns=@JoinColumn(name="selection_id"))
-    @Column(name="field")
-    @Enumerated(EnumType.STRING)
-    private List<Field> fields;
-    private String location;
 }
