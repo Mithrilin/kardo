759c49a77e5c0fdd2fbfe158d8ba29f3b1d1023b
Roman <romanukyan86@yandex.ru>
2024-08-02 23:23:36 +0300

    добавить класс EventSpectatorRequestDto;

---
+++ src/main/java/com/kardoaward/kardo/spectator_request/event_spectator_request/model/dto/EventSpectatorRequestDto.java
@@ -0,0 +1,17 @@
+package com.kardoaward.kardo.spectator_request.event_spectator_request.model.dto;
+
+import com.kardoaward.kardo.event.model.dto.EventDto;
+import com.kardoaward.kardo.spectator_request.model.dto.SpectatorRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventSpectatorRequestDto extends SpectatorRequestDto {
+
+    private EventDto eventDto;
+}
