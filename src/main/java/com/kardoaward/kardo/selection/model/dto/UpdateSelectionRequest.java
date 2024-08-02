a5e975c9fa4d904b390e7503c69efec7c2e002f0
Roman <romanukyan86@yandex.ru>
2024-08-02 19:44:49 +0300

    добавить класс UpdateOfflineSelectionRequest наследник от UpdateSelectionRequest;

--- src/main/java/com/kardoaward/kardo/selection/model/dto/UpdateSelectionRequest.java
+++ src/main/java/com/kardoaward/kardo/selection/model/dto/UpdateSelectionRequest.java
@@ -1,8 +1,6 @@
 package com.kardoaward.kardo.selection.model.dto;
 
-import com.kardoaward.kardo.enums.Field;
 import com.kardoaward.kardo.enums.Status;
-import com.kardoaward.kardo.selection.model.enums.SelectionType;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -10,7 +8,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,13 +16,9 @@ public class UpdateSelectionRequest {
 
     @Size(min = 2, max = 250, message = "Длина title должна быть в диапазоне от 2 до 250 символов.")
     private String title;
-    private SelectionType selectionType;
     @Future(message = "SelectionStart должен быть в будущем.")
     private LocalDate selectionStart;
     @Future(message = "SelectionEnd должен быть в будущем.")
     private LocalDate selectionEnd;
     private Status status;
-    private List<Field> fields;
-    @Size(min = 2, max = 250, message = "Длина location должна быть в диапазоне от 2 до 250 символов.")
-    private String location;
 }
