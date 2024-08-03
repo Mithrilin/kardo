61be655dcb35367c16dbcaf8292ca09fb735a78d
Roman <romanukyan86@yandex.ru>
2024-08-03 11:40:53 +0300

    добавить класс VideoClipServiceImpl;

---
+++ src/main/java/com/kardoaward/kardo/video_clip/service/VideoClipServiceImpl.java
@@ -0,0 +1,11 @@
+package com.kardoaward.kardo.video_clip.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class VideoClipServiceImpl implements VideoClipService {
+}
