3d1512292c76733c6e57b8606b0936a07a3739c5
Roman <romanukyan86@yandex.ru>
2024-08-03 12:38:49 +0300

    добавить метод updateVideoClip в VideoClipMapper;

--- src/main/java/com/kardoaward/kardo/video_clip/mapper/VideoClipMapper.java
+++ src/main/java/com/kardoaward/kardo/video_clip/mapper/VideoClipMapper.java
@@ -4,9 +4,11 @@ import com.kardoaward.kardo.user.mapper.UserMapper;
 import com.kardoaward.kardo.user.model.User;
 import com.kardoaward.kardo.video_clip.model.VideoClip;
 import com.kardoaward.kardo.video_clip.model.dto.NewVideoClipRequest;
+import com.kardoaward.kardo.video_clip.model.dto.UpdateVideoClipRequest;
 import com.kardoaward.kardo.video_clip.model.dto.VideoClipDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface VideoClipMapper {
@@ -17,4 +19,6 @@ public interface VideoClipMapper {
 
     @Mapping(source = "returnedVideoClip.creator", target = "creatorDto")
     VideoClipDto videoClipToVideoClipDto(VideoClip returnedVideoClip);
+
+    void updateVideoClip(UpdateVideoClipRequest request, @MappingTarget VideoClip videoClip);
 }
