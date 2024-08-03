43434e23a41d3266f96594a5e82a997d3d056673
Roman <romanukyan86@yandex.ru>
2024-08-03 12:01:52 +0300

    добавить метод newVideoClipRequestToVideoClip в VideoClipMapper;

--- src/main/java/com/kardoaward/kardo/video_clip/mapper/VideoClipMapper.java
+++ src/main/java/com/kardoaward/kardo/video_clip/mapper/VideoClipMapper.java
@@ -1,7 +1,15 @@
 package com.kardoaward.kardo.video_clip.mapper;
 
+import com.kardoaward.kardo.user.model.User;
+import com.kardoaward.kardo.video_clip.model.VideoClip;
+import com.kardoaward.kardo.video_clip.model.dto.NewVideoClipRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface VideoClipMapper {
+
+    @Mapping(target = "id", constant = "0L")
+    @Mapping(source = "user", target = "creator")
+    VideoClip newVideoClipRequestToVideoClip(NewVideoClipRequest request, User user);
 }
