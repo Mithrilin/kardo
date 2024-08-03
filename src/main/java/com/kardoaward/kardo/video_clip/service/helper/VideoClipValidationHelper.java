b14597ab5d15006ad1a0533066c31dbdf374cc9f
Roman <romanukyan86@yandex.ru>
2024-08-03 11:52:31 +0300

    добавить класс VideoClipValidationHelper;

---
+++ src/main/java/com/kardoaward/kardo/video_clip/service/helper/VideoClipValidationHelper.java
@@ -0,0 +1,13 @@
+package com.kardoaward.kardo.video_clip.service.helper;
+
+import com.kardoaward.kardo.exception.BadRequestException;
+import com.kardoaward.kardo.video_clip.model.dto.NewVideoClipRequest;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
+public class VideoClipValidationHelper {
+
+
+}
