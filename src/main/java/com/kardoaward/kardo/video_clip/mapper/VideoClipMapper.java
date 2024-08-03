e03c9568190723bb302e295100a73d9de6e05c52
Roman <romanukyan86@yandex.ru>
2024-08-03 12:06:32 +0300

    добавить метод videoClipToVideoClipDto в VideoClipMapper;

--- src/main/java/com/kardoaward/kardo/video_clip/mapper/VideoClipMapper.java
+++ src/main/java/com/kardoaward/kardo/video_clip/mapper/VideoClipMapper.java
@@ -1,15 +1,20 @@
 package com.kardoaward.kardo.video_clip.mapper;
 
+import com.kardoaward.kardo.user.mapper.UserMapper;
 import com.kardoaward.kardo.user.model.User;
 import com.kardoaward.kardo.video_clip.model.VideoClip;
 import com.kardoaward.kardo.video_clip.model.dto.NewVideoClipRequest;
+import com.kardoaward.kardo.video_clip.model.dto.VideoClipDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface VideoClipMapper {
 
     @Mapping(target = "id", constant = "0L")
     @Mapping(source = "user", target = "creator")
     VideoClip newVideoClipRequestToVideoClip(NewVideoClipRequest request, User user);
+
+    @Mapping(source = "returnedVideoClip.creator", target = "creatorDto")
+    VideoClipDto videoClipToVideoClipDto(VideoClip returnedVideoClip);
 }
