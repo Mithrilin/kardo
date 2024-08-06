1d4507f5656b680cab01f5d690ae5fb595d8e4c7
Roman <romanukyan86@yandex.ru>
2024-08-07 00:12:32 +0300

    добавить интерфейс SelectionSpectatorRequestMapper;

---
+++ src/main/java/com/kardoaward/kardo/spectator_request/selection_spectator_request/mapper/SelectionSpectatorRequestMapper.java
@@ -0,0 +1,7 @@
+package com.kardoaward.kardo.spectator_request.selection_spectator_request.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SelectionSpectatorRequestMapper {
+}
