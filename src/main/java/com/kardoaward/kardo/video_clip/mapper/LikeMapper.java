c44d26bb8f1e1b7e9ebfdd9622bbe96ed26709df
Roman <romanukyan86@yandex.ru>
2024-08-06 19:18:43 +0300

    добавить интерфейс LikeMapper;

---
+++ src/main/java/com/kardoaward/kardo/video_clip/mapper/LikeMapper.java
@@ -0,0 +1,13 @@
+package com.kardoaward.kardo.video_clip.mapper;
+
+import com.kardoaward.kardo.user.model.User;
+import com.kardoaward.kardo.video_clip.model.VideoClip;
+import com.kardoaward.kardo.video_clip.model.like.Like;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface LikeMapper {
+
+
+}
