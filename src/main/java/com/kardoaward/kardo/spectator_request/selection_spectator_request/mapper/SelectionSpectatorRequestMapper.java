8d253707e15b9602cf6b21567e6ef4eb0b348413
Roman <romanukyan86@yandex.ru>
2024-08-07 00:27:42 +0300

    добавить метод spectatorRequestToSpectatorRequestDto в SelectionSpectatorRequestMapper;

--- src/main/java/com/kardoaward/kardo/spectator_request/selection_spectator_request/mapper/SelectionSpectatorRequestMapper.java
+++ src/main/java/com/kardoaward/kardo/spectator_request/selection_spectator_request/mapper/SelectionSpectatorRequestMapper.java
@@ -3,6 +3,7 @@ package com.kardoaward.kardo.spectator_request.selection_spectator_request.mappe
 import com.kardoaward.kardo.selection.offline_selection.mapper.OfflineSelectionMapper;
 import com.kardoaward.kardo.selection.offline_selection.model.OfflineSelection;
 import com.kardoaward.kardo.spectator_request.selection_spectator_request.model.SelectionSpectatorRequest;
+import com.kardoaward.kardo.spectator_request.selection_spectator_request.model.dto.SelectionSpectatorRequestDto;
 import com.kardoaward.kardo.user.mapper.UserMapper;
 import com.kardoaward.kardo.user.model.User;
 import org.mapstruct.Mapper;
@@ -15,4 +16,8 @@ public interface SelectionSpectatorRequestMapper {
     @Mapping(source = "user", target = "requester")
     @Mapping(source = "offlineSelection", target = "selection")
     SelectionSpectatorRequest newSpectatorRequestToSpectatorRequest(OfflineSelection offlineSelection, User user);
+
+    @Mapping(source = "spectatorRequest.selection", target = "selectionDto")
+    @Mapping(source = "spectatorRequest.requester", target = "requesterDto")
+    SelectionSpectatorRequestDto spectatorRequestToSpectatorRequestDto(SelectionSpectatorRequest spectatorRequest);
 }
