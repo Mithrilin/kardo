3866f397274094762df2d7b2f7f91d9cabfa2b86
Roman <romanukyan86@yandex.ru>
2024-08-07 00:58:53 +0300

    добавить класс SelectionSpectatorRequestAdminController;

---
+++ src/main/java/com/kardoaward/kardo/spectator_request/selection_spectator_request/controller/admin/SelectionSpectatorRequestAdminController.java
@@ -0,0 +1,15 @@
+package com.kardoaward.kardo.spectator_request.selection_spectator_request.controller.admin;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/admin/spectators/selection")
+@Validated
+public class SelectionSpectatorRequestAdminController {
+}
