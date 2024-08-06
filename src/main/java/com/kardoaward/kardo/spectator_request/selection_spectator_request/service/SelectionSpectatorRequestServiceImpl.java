36e639c3f3937e8626ca5fc0eaeed0981da9260f
Roman <romanukyan86@yandex.ru>
2024-08-06 23:56:28 +0300

    добавить класс SelectionSpectatorRequestServiceImpl;

---
+++ src/main/java/com/kardoaward/kardo/spectator_request/selection_spectator_request/service/SelectionSpectatorRequestServiceImpl.java
@@ -0,0 +1,11 @@
+package com.kardoaward.kardo.spectator_request.selection_spectator_request.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class SelectionSpectatorRequestServiceImpl implements SelectionSpectatorRequestService {
+}
