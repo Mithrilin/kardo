7974d02f530d4a864cb563cb70456599fd640be7
Roman <romanukyan86@yandex.ru>
2024-08-07 01:13:49 +0300

    добавить метод findBySelection_Id в SelectionSpectatorRequestRepository;

--- src/main/java/com/kardoaward/kardo/spectator_request/selection_spectator_request/repository/SelectionSpectatorRequestRepository.java
+++ src/main/java/com/kardoaward/kardo/spectator_request/selection_spectator_request/repository/SelectionSpectatorRequestRepository.java
@@ -1,7 +1,11 @@
 package com.kardoaward.kardo.spectator_request.selection_spectator_request.repository;
 
 import com.kardoaward.kardo.spectator_request.selection_spectator_request.model.SelectionSpectatorRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SelectionSpectatorRequestRepository extends JpaRepository<SelectionSpectatorRequest, Long> {
+
+    Page<SelectionSpectatorRequest> findBySelection_Id(Long selectionId, PageRequest pageRequest);
 }
