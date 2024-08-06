cf1a1fd890e4b96ae11812d46d77abc56d418ded
Roman <romanukyan86@yandex.ru>
2024-08-07 00:21:39 +0300

    добавить интерфейс SelectionSpectatorRequestRepository;

---
+++ src/main/java/com/kardoaward/kardo/spectator_request/selection_spectator_request/repository/SelectionSpectatorRequestRepository.java
@@ -0,0 +1,7 @@
+package com.kardoaward.kardo.spectator_request.selection_spectator_request.repository;
+
+import com.kardoaward.kardo.spectator_request.selection_spectator_request.model.SelectionSpectatorRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SelectionSpectatorRequestRepository extends JpaRepository<SelectionSpectatorRequest, Long> {
+}
