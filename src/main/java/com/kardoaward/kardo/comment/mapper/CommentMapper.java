44eba154602731641752e8f81ebe2798fcf6245b
Roman <romanukyan86@yandex.ru>
2024-08-07 16:07:37 +0300

    добавить метод commentToCommentDto в CommentMapper;

--- src/main/java/com/kardoaward/kardo/comment/mapper/CommentMapper.java
+++ src/main/java/com/kardoaward/kardo/comment/mapper/CommentMapper.java
@@ -1,17 +1,24 @@
 package com.kardoaward.kardo.comment.mapper;
 
 import com.kardoaward.kardo.comment.model.Comment;
+import com.kardoaward.kardo.comment.model.dto.CommentDto;
 import com.kardoaward.kardo.comment.model.dto.NewCommentRequest;
+import com.kardoaward.kardo.user.mapper.UserMapper;
 import com.kardoaward.kardo.user.model.User;
+import com.kardoaward.kardo.video_clip.mapper.VideoClipMapper;
 import com.kardoaward.kardo.video_clip.model.VideoClip;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {UserMapper.class, VideoClipMapper.class})
 public interface CommentMapper {
 
     @Mapping(target = "id", constant = "0L")
     @Mapping(source = "user", target = "author")
     @Mapping(source = "videoClip", target = "videoClip")
     Comment newCommentRequestToComment(NewCommentRequest newCommentRequest, User user, VideoClip videoClip);
+
+    @Mapping(source = "comment.author", target = "authorDto")
+    @Mapping(source = "comment.videoClip", target = "videoClipDto")
+    CommentDto commentToCommentDto(Comment comment);
 }
