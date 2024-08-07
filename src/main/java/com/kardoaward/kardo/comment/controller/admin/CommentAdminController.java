b2191924023748a800af078e4e5c22ae7ae6d989
Roman <romanukyan86@yandex.ru>
2024-08-07 15:40:29 +0300

    добавить класс CommentAdminController;

---
+++ src/main/java/com/kardoaward/kardo/comment/controller/admin/CommentAdminController.java
@@ -0,0 +1,15 @@
+package com.kardoaward.kardo.comment.controller.admin;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/admin/comments")
+@Validated
+public class CommentAdminController {
+}
