4e4112eac719775d7c4031920d4c7434bc044bab
Roman <romanukyan86@yandex.ru>
2024-08-07 13:05:01 +0300

    добавить метод isUserRequester в EventSpectatorRequestValidationHelper;

--- src/main/java/com/kardoaward/kardo/spectator_request/event_spectator_request/service/helper/EventSpectatorRequestValidationHelper.java
+++ src/main/java/com/kardoaward/kardo/spectator_request/event_spectator_request/service/helper/EventSpectatorRequestValidationHelper.java
@@ -1,5 +1,6 @@
 package com.kardoaward.kardo.spectator_request.event_spectator_request.service.helper;
 
+import com.kardoaward.kardo.exception.NotValidException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class EventSpectatorRequestValidationHelper {
+
+    public void isUserRequester(Long userId, Long requesterId) {
+        if (!userId.equals(requesterId)) {
+            log.error("Пользователь с ИД {} не является создателем заявки зрителя мероприятия.", userId);
+            throw new NotValidException(String.format("Пользователь с ИД %d не является создателем заявки зрителя " +
+                    "мероприятия.", userId));
+        }
+    }
 }
