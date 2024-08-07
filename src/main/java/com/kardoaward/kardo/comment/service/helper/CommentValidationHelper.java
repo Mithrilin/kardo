8c64a40456c87a00f6d49a732d5825cea07e3369
Roman <romanukyan86@yandex.ru>
2024-08-07 15:51:28 +0300

    добавить метод isUserAuthor в CommentValidationHelper;

--- src/main/java/com/kardoaward/kardo/comment/service/helper/CommentValidationHelper.java
+++ src/main/java/com/kardoaward/kardo/comment/service/helper/CommentValidationHelper.java
@@ -1,5 +1,6 @@
 package com.kardoaward.kardo.comment.service.helper;
 
+import com.kardoaward.kardo.exception.NotValidException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class CommentValidationHelper {
+
+    public void isUserAuthor(Long requestorId, Long authorId) {
+        if (!requestorId.equals(authorId)) {
+            log.error("Пользователь с ИД {} не является создателем комментария.", requestorId);
+            throw new NotValidException(String.format("Пользователь с ИД %d не является создателем комментария.",
+                    requestorId));
+        }
+    }
 }
