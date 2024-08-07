6858c0e969bad15ed84f2c96bdf1e638280b7bda
Roman <romanukyan86@yandex.ru>
2024-08-07 16:02:35 +0300

    добавить метод newCommentRequestToComment в CommentMapper;

--- src/main/java/com/kardoaward/kardo/comment/mapper/CommentMapper.java
+++ src/main/java/com/kardoaward/kardo/comment/mapper/CommentMapper.java
@@ -1,7 +1,17 @@
 package com.kardoaward.kardo.comment.mapper;
 
+import com.kardoaward.kardo.comment.model.Comment;
+import com.kardoaward.kardo.comment.model.dto.NewCommentRequest;
+import com.kardoaward.kardo.user.model.User;
+import com.kardoaward.kardo.video_clip.model.VideoClip;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
+
+    @Mapping(target = "id", constant = "0L")
+    @Mapping(source = "user", target = "author")
+    @Mapping(source = "videoClip", target = "videoClip")
+    Comment newCommentRequestToComment(NewCommentRequest newCommentRequest, User user, VideoClip videoClip);
 }
