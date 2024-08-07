db7f90cd515cdf578961d69289473893d76160c4
Roman <romanukyan86@yandex.ru>
2024-08-07 15:39:54 +0300

    добавить класс CommentController;

---
+++ src/main/java/com/kardoaward/kardo/comment/controller/CommentController.java
@@ -0,0 +1,15 @@
+package com.kardoaward.kardo.comment.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/comments")
+@Validated
+public class CommentController {
+}
