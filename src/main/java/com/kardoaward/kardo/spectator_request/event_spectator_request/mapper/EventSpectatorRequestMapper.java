6b80d03eb26922c0adac2b81dc73a4b1a7331f8f
Roman <romanukyan86@yandex.ru>
2024-08-07 13:12:29 +0300

    добавить метод newSpectatorRequestToSpectatorRequest в EventSpectatorRequestMapper;

--- src/main/java/com/kardoaward/kardo/spectator_request/event_spectator_request/mapper/EventSpectatorRequestMapper.java
+++ src/main/java/com/kardoaward/kardo/spectator_request/event_spectator_request/mapper/EventSpectatorRequestMapper.java
@@ -1,7 +1,16 @@
 package com.kardoaward.kardo.spectator_request.event_spectator_request.mapper;
 
+import com.kardoaward.kardo.event.model.Event;
+import com.kardoaward.kardo.spectator_request.event_spectator_request.model.EventSpectatorRequest;
+import com.kardoaward.kardo.user.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EventSpectatorRequestMapper {
+
+    @Mapping(target = "id", constant = "0L")
+    @Mapping(source = "user", target = "requester")
+    @Mapping(source = "event", target = "event")
+    EventSpectatorRequest newSpectatorRequestToSpectatorRequest(Event event, User user);
 }
