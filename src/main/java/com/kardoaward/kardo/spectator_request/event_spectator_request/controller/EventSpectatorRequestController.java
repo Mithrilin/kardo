bd6fe82338661a96c20bdc2aa031b67893c45499
Roman <romanukyan86@yandex.ru>
2024-08-07 12:55:34 +0300

    добавить класс EventSpectatorRequestController;

---
+++ src/main/java/com/kardoaward/kardo/spectator_request/event_spectator_request/controller/EventSpectatorRequestController.java
@@ -0,0 +1,15 @@
+package com.kardoaward.kardo.spectator_request.event_spectator_request.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/spectators/event")
+@Validated
+public class EventSpectatorRequestController {
+}
