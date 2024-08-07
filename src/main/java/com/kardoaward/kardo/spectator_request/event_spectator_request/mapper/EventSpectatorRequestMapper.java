d2e93b3f3ff85d0aa18a377cead1d16b5fce60c5
Roman <romanukyan86@yandex.ru>
2024-08-07 13:09:16 +0300

    добавить интерфейс EventSpectatorRequestMapper;

---
+++ src/main/java/com/kardoaward/kardo/spectator_request/event_spectator_request/mapper/EventSpectatorRequestMapper.java
@@ -0,0 +1,7 @@
+package com.kardoaward.kardo.spectator_request.event_spectator_request.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface EventSpectatorRequestMapper {
+}
