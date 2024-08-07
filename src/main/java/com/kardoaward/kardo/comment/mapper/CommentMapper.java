8e70df4af81c552250929f64c8a58b63d8e5d57a
Roman <romanukyan86@yandex.ru>
2024-08-07 16:31:53 +0300

    добавить метод updateComment в CommentMapper;

--- src/main/java/com/kardoaward/kardo/comment/mapper/CommentMapper.java
+++ src/main/java/com/kardoaward/kardo/comment/mapper/CommentMapper.java
@@ -3,12 +3,14 @@ package com.kardoaward.kardo.comment.mapper;
 import com.kardoaward.kardo.comment.model.Comment;
 import com.kardoaward.kardo.comment.model.dto.CommentDto;
 import com.kardoaward.kardo.comment.model.dto.NewCommentRequest;
+import com.kardoaward.kardo.comment.model.dto.UpdateCommentRequest;
 import com.kardoaward.kardo.user.mapper.UserMapper;
 import com.kardoaward.kardo.user.model.User;
 import com.kardoaward.kardo.video_clip.mapper.VideoClipMapper;
 import com.kardoaward.kardo.video_clip.model.VideoClip;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", uses = {UserMapper.class, VideoClipMapper.class})
 public interface CommentMapper {
@@ -21,4 +23,6 @@ public interface CommentMapper {
     @Mapping(source = "comment.author", target = "authorDto")
     @Mapping(source = "comment.videoClip", target = "videoClipDto")
     CommentDto commentToCommentDto(Comment comment);
+
+    void updateComment(UpdateCommentRequest request, @MappingTarget Comment comment);
 }
