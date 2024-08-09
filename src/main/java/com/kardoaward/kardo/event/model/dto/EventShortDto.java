66978b66b9fe41930a4f67388420a6390adfd4f4
Roman <romanukyan86@yandex.ru>
2024-08-09 20:19:06 +0300

    добавить класс EventShortDto;

---
+++ src/main/java/com/kardoaward/kardo/event/model/dto/EventShortDto.java
@@ -0,0 +1,22 @@
+package com.kardoaward.kardo.event.model.dto;
+
+import com.kardoaward.kardo.enums.Status;
+import com.kardoaward.kardo.grand_competition.model.dto.GrandCompetitionDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventShortDto {
+
+    private Long id;
+    private String title;
+    private GrandCompetitionDto competitionDto;
+    private LocalDateTime eventStart;
+    private LocalDateTime eventEnd;
+    private Status status;
+}
