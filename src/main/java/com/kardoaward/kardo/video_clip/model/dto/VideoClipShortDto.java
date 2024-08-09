db1baa8cc533a5189ac9ab8a1cf00f81ee326347
Roman <romanukyan86@yandex.ru>
2024-08-09 20:19:17 +0300

    добавить класс VideoClipShortDto;

---
+++ src/main/java/com/kardoaward/kardo/video_clip/model/dto/VideoClipShortDto.java
@@ -0,0 +1,21 @@
+package com.kardoaward.kardo.video_clip.model.dto;
+
+import com.kardoaward.kardo.user.model.dto.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoClipShortDto {
+
+    private Long id;
+    private LocalDateTime publicationTime;
+    private Set<String> hashtags;
+    private UserDto creatorDto;
+    private Integer likesCount;
+}
