da66241623f341f9b09f091fe39579fc50e203e5
Roman <romanukyan86@yandex.ru>
2024-08-11 19:52:22 +0300

    добавить класс AuthenticationRestController;

---
+++ src/main/java/com/kardoaward/kardo/security/controller/AuthenticationRestController.java
@@ -0,0 +1,11 @@
+package com.kardoaward.kardo.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users/auth")
+public class AuthenticationRestController {
+
+
+}
