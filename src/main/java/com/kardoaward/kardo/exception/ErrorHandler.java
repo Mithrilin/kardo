5e0830c2de1b159e1fc5f03c1ab486acefe65798
Roman <romanukyan86@yandex.ru>
2024-08-12 00:50:19 +0300

    добавить класс JwtAuthenticationException;

--- src/main/java/com/kardoaward/kardo/exception/ErrorHandler.java
+++ src/main/java/com/kardoaward/kardo/exception/ErrorHandler.java
@@ -35,6 +35,13 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public Map<String, String> handleAuthenticationException(final JwtAuthenticationException e) {
+        log.error("Получен статус 401 UNAUTHORIZED. {}", e.getMessage(), e);
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleAllException(final Exception e) {
         log.error("Получен статус 500 Internal Server Error. {}", e.getMessage(), e);
