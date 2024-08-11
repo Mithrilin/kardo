5e6350b0e74253ddc2c4f6eb76b104f0fe5651e7
Roman <romanukyan86@yandex.ru>
2024-08-11 13:21:17 +0300

    добавить методы в SecurityConfig;

--- src/main/java/com/kardoaward/kardo/config/SecurityConfig.java
+++ src/main/java/com/kardoaward/kardo/config/SecurityConfig.java
@@ -1,13 +1,72 @@
 package com.kardoaward.kardo.config;
 
+import com.kardoaward.kardo.user.service.MyUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new MyUserDetailsService();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // Отключаем csrf
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // Настраиваем вход на регистрацию "/users/reg"
+        http.authorizeHttpRequests(requestMatcherRegistry ->
+                requestMatcherRegistry
+                        .requestMatchers("/users/reg")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+        );
+
+        // Configure form-based login
+        http.formLogin(Customizer.withDefaults());
+
+        // Configure logout functionality
+        http.logout(logoutConfigurer ->
+                logoutConfigurer
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutUrl("/logout")
+                        .permitAll()
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
