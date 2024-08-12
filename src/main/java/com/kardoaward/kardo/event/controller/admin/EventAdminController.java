bb0e7ff453a5dd97dc978324be502eeefd4a0a6f
Roman <romanukyan86@yandex.ru>
2024-08-13 01:31:23 +0300

    добавить аннотации сваггера к EventAdminController;

--- src/main/java/com/kardoaward/kardo/event/controller/admin/EventAdminController.java
+++ src/main/java/com/kardoaward/kardo/event/controller/admin/EventAdminController.java
@@ -5,6 +5,13 @@ import com.kardoaward.kardo.event.model.dto.NewEventRequest;
 import com.kardoaward.kardo.event.model.dto.UpdateEventRequest;
 import com.kardoaward.kardo.event.service.EventService;
 import com.kardoaward.kardo.event.service.helper.EventValidationHelper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -26,39 +33,80 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/admin/events")
 @Validated
+@Tag(name="Мероприятия: Admin.", description="API администратора для работы с мероприятиями.")
 public class EventAdminController {
 
     private final EventService eventService;
 
     private final EventValidationHelper eventValidationHelper;
 
+    @Operation(summary = "Добавление администратором нового мероприятия к Гранд-соревнованию.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Мероприятие добавлено.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EventDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Запрос составлен некорректно", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Гранд-соревнование не найдено", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера", content = @Content),})
     @PostMapping
     @Secured("ADMIN")
-    public EventDto createEvent(@RequestBody @Valid NewEventRequest newEventRequest) {
+    public EventDto createEvent(@Parameter(description = "Данные добавляемого мероприятия")
+                                @RequestBody @Valid NewEventRequest newEventRequest) {
         log.info("Добавление администратором нового мероприятия к гранд-соревнованию с ИД {}.",
                 newEventRequest.getCompetitionId());
         eventValidationHelper.isNewEventDateValid(newEventRequest);
         return eventService.addEvent(newEventRequest);
     }
 
+    @Operation(summary = "Добавление администратором логотипа к мероприятию.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Логотип к мероприятию добавлен.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EventDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Запрос составлен некорректно", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Мероприятие не найдено", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера", content = @Content),})
     @PostMapping("/{eventId}/logo")
     @Secured("ADMIN")
-    public EventDto addEventLogo(@PathVariable @Positive Long eventId,
+    public EventDto addEventLogo(@Parameter(description = "id мероприятия")
+                                 @PathVariable @Positive Long eventId,
+                                 @Parameter(description = "MultipartFile с логотипом")
                                  @RequestParam("image") MultipartFile file) {
         log.info("Добавление администратором логотипа к мероприятию с ИД {}.", eventId);
         return eventService.addEventLogo(eventId, file);
     }
 
+    @Operation(summary = "Удаление администратором мероприятия по ИД.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Мероприятие удалено.", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Запрос составлен некорректно", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Мероприятие не найдено", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера", content = @Content),})
     @DeleteMapping("/{eventId}")
     @Secured("ADMIN")
-    public void deleteEventById(@PathVariable @Positive Long eventId) {
+    public void deleteEventById(@Parameter(description = "id мероприятия")
+                                @PathVariable @Positive Long eventId) {
         log.info("Удаление администратором мероприятия с ИД {}.", eventId);
         eventService.deleteEventById(eventId);
     }
 
+    @Operation(summary = "Обновление администратором мероприятия по ИД.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Мероприятие обновлено.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EventDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Запрос составлен некорректно", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Мероприятие не найдено", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера", content = @Content),})
     @PatchMapping("/{eventId}")
     @Secured("ADMIN")
-    public EventDto updateEventById(@PathVariable @Positive Long eventId,
+    public EventDto updateEventById(@Parameter(description = "id мероприятия")
+                                    @PathVariable @Positive Long eventId,
+                                    @Parameter(description = "Данные обновляемого мероприятия")
                                     @RequestBody @Valid UpdateEventRequest request) {
         log.info("Обновление администратором мероприятия с ИД {}.", eventId);
         return eventService.updateEventById(eventId, request);
