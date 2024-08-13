20be23cc17db3a82e35d65fb71f1332d0cd6f7e9
Roman <romanukyan86@yandex.ru>
2024-08-13 03:15:21 +0300

    добавить аннотации сваггера к EventSpectatorRequestAdminController;

--- src/main/java/com/kardoaward/kardo/spectator_request/event_spectator_request/controller/admin/EventSpectatorRequestAdminController.java
+++ src/main/java/com/kardoaward/kardo/spectator_request/event_spectator_request/controller/admin/EventSpectatorRequestAdminController.java
@@ -4,6 +4,13 @@ import com.kardoaward.kardo.spectator_request.event_spectator_request.model.dto.
 import com.kardoaward.kardo.spectator_request.event_spectator_request.service.EventSpectatorRequestService;
 import com.kardoaward.kardo.spectator_request.model.dto.update.SpectatorRequestStatusUpdateRequest;
 import com.kardoaward.kardo.spectator_request.model.dto.update.SpectatorRequestStatusUpdateResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Positive;
@@ -26,25 +33,47 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/spectators/event")
 @Validated
+@Tag(name="Заявка зрителя на мероприятие: Admin.", description="API администратора для работы с заявками зрителей " +
+        "на мероприятие.")
 public class EventSpectatorRequestAdminController {
 
     private final EventSpectatorRequestService service;
 
+    @Operation(summary = "Получение списка заявок зрителей по ИД мероприятия.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Запрос составлен некорректно", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Мероприятие не найдено", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера", content = @Content)})
     @GetMapping("/events/{eventId}")
     @Secured("ADMIN")
-    public List<EventSpectatorRequestDto> getEventSpectatorRequestByEventId(@PathVariable @Positive Long eventId,
-                                                                            @RequestParam(defaultValue = "0")
-                                                                            @Min(0) int from,
-                                                                            @RequestParam(defaultValue = "10")
-                                                                            @Positive int size) {
+    public List<EventSpectatorRequestDto> getEventSpectatorRequestByEventId(
+                                                          @Parameter(description = "id мероприятия")
+                                                          @PathVariable @Positive Long eventId,
+                                                          @Parameter(description = "Количество элементов, которые " +
+                                                          "нужно пропустить для формирования текущего набора")
+                                                          @RequestParam(defaultValue = "0") @Min(0) int from,
+                                                          @Parameter(description = "Количество элементов в наборе")
+                                                          @RequestParam(defaultValue = "10") @Positive int size) {
         log.info("Возвращение администратору списка заявок зрителей к мероприятию с ИД {}.", eventId);
         return service.getEventSpectatorRequestByEventId(eventId, from, size);
     }
 
+    @Operation(summary = "Обновление администратором статуса заявок зрителей к мероприятию.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Заявки обновлены.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = SpectatorRequestStatusUpdateResult.class)) }),
+            @ApiResponse(responseCode = "400", description = "Запрос составлен некорректно", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Пользователь не найден", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера", content = @Content)})
     @PatchMapping("/events/{eventId}")
     @Secured("ADMIN")
     public SpectatorRequestStatusUpdateResult updateEventSpectatorRequestStatusByEventId(
+                                                    @Parameter(description = "id мероприятия")
                                                     @PathVariable @Positive Long eventId,
+                                                    @Parameter(description = "Данные обновляемых заявок")
                                                     @RequestBody @Valid SpectatorRequestStatusUpdateRequest request) {
         log.info("Обновление администратором статуса заявок зрителей к мероприятию с ИД {}.", eventId);
         return service.updateEventSpectatorRequestStatusByEventId(eventId, request);
