6cd602b4fb3feaf73bf33dddcd2cf3adebc1a2a9
Roman <romanukyan86@yandex.ru>
2024-08-22 12:20:08 +0300

    добавить класс FileManager;

---
+++ src/main/java/com/kardoaward/kardo/media_file/FileManager.java
@@ -0,0 +1,25 @@
+package com.kardoaward.kardo.media_file;
+
+import com.kardoaward.kardo.exception.FileContentException;
+import com.kardoaward.kardo.user.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
+@Service
+public class FileManager {
+
+    private final String FOLDER_PATH;
+
+    public FileManager(@Value("${folder.path}") String folderPath) {
+        FOLDER_PATH = folderPath;
+    }
+
+
+}
